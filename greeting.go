8a978a958815fd64b197c4bd35592d1aa283239c
Gary Xue <gary.xuehong@gmail.com>
2025-06-03 17:40:28 +1000

    fix

--- greeting.go
+++ greeting.go
@@ -3,10 +3,10 @@ package greeting
 import "fmt"
 import "errors"
 
-func Hello(name string) (string , error){
-  if(name =="gary") {
-    return "", errors.New("empty name")
-  }
-  msg := fmt.Sprintf("Hi %v", name)
-  return msg, nil
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	msg := fmt.Sprintf("Hi %v", name)
+	return msg, nil
 }
