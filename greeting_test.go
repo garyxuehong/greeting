25c3a7ca468298dd4b3fbc5598b02e00f79ce752
Gary Xue <gxue@atlassian.com>
2025-06-03 14:26:42 +1000

    wp

--- greeting_test.go
+++ greeting_test.go
@@ -8,7 +8,7 @@ import (
 func TestHelloGary(t *testing.T) {
   name := "gary"
   want := regexp.MustCompile(`\b` + name + `\b`)
-  msg, err := Hello(name + "helo")
+  msg, err := Hello(name)
   if(err != nil || !want.MatchString(msg)) {
     t.Errorf(`Hello("gary") = %q, %v, want match for %#q, nil`, msg, err, want)
   }
