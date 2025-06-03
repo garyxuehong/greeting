84724d39b711f344a8ce372a4b9c449b6fb49273
Gary Xue <gary.xuehong@gmail.com>
2025-06-03 14:36:04 +1000

    gary

--- greeting.go
+++ greeting.go
@@ -4,7 +4,7 @@ import "fmt"
 import "errors"
 
 func Hello(name string) (string , error){
-  if(name =="") {
+  if(name =="gary") {
     return "", errors.New("empty name")
   }
   msg := fmt.Sprintf("Hi %v", name)
